19fd5f0cdfc8844e5160a7d5c0a795779ac2a1ee
amargue <antoniojesus.marchena@iesdonana.org>
2023-05-29 18:59:31 +0200

    ya se puede comentar y arreglar el cambio de pagia en vista_detalle

--- public/vista_detalle.php
+++ public/vista_detalle.php
@@ -15,7 +15,7 @@ session_start() ?>
     <title>Vista Detalle</title>
 
     <script>
-        function cambiar(el, articulo_id, usuario_id) {
+        function comentar(el, articulo_id, usuario_id) {
             el.preventDefault();
             const oculto_art = document.getElementById('ocultoId');
             oculto_art.setAttribute('value', articulo_id);
@@ -99,7 +99,7 @@ session_start() ?>
                             <td colspan="2" class="border-t border-gray-300 py-4 text-center bg-gray-200">Precio:</td>
                             <td class="border-t border-gray-300 py-4 text-center"><?= $producto->getPrecio() ?> €</td>
                             <td colspan="2" class="border-t border-gray-300 py-4 text-center bg-gray-200">
-                                <a href="/insertar_en_carrito.php?id=<?= $producto->getId() ?>&categoria=<?= $producto->getCategoriaId() ?>&etiquetas=<?= $producto->getEtiquetaNombre() ?>" class="inline-flex items-center py-2 px-3.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <a href="/insertar_en_carrito.php?id=<?= $producto_id ?>&categoria=<?= $producto->getCategoriaId() ?>&etiquetas=<?= $producto->getEtiquetaNombre() ?>" class="inline-flex items-center py-2 px-3.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Añadir al carrito
                                     <svg aria-hidden="true" class="ml-3 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -118,7 +118,7 @@ session_start() ?>
                                                                             FROM valoraciones
                                                                             WHERE usuario_id = :usuario_id AND articulo_id = :articulo_id
                                                                             ORDER BY created_at DESC LIMIT 1");
-                                        $sent_valoraciones->execute(['usuario_id' => $usuario_id, 'articulo_id' => $producto->getId()]);
+                                        $sent_valoraciones->execute(['usuario_id' => $usuario_id, 'articulo_id' => $producto_id]);
                                         $valoracion_usuario = $sent_valoraciones->fetch(PDO::FETCH_ASSOC);
                                         ?>
                                         <select name="valoracion" id="valoracion">
@@ -127,7 +127,7 @@ session_start() ?>
                                                 <option value="<?= $i ?>" <?= ($valoracion_usuario['valoracion'] == $i) ? 'selected' : '' ?>><?= $i ?></option>
                                             <?php endfor ?>
                                         </select>
-                                        <input type="hidden" name="articulo_id" value="<?= $producto->getId() ?>">
+                                        <input type="hidden" name="articulo_id" value="<?= $producto_id ?>">
                                         <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
                                         <?php if (!\App\Tablas\Usuario::esta_logueado()) : ?>
                                             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" disabled>Votar</button>
@@ -142,9 +142,11 @@ session_start() ?>
                             <td>
                                 <?php if (\App\Tablas\Usuario::esta_logueado()) : ?>
                                     <form action="comentar_articulo.php" method="POST" class="inline">
-                                        <input type="hidden" name="articulo_id" value="<?= $fila['id'] ?>">
+                                        <input type="hidden" name="articulo_id" value="<?= $producto_id ?>">
                                         <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
-                                        <button type="submit" onclick="cambiar(event, <?= $fila['id'] ?>, <?= $usuario_id ?>)" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900" data-modal-toggle="insertar_comentario">Comentar</button>
+                                        <button type="submit" onclick="comentar(event, <?= $producto_id ?>, <?= $usuario_id ?>)"
+                                            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"
+                                            data-modal-toggle="comentar">Comentar</button>
                                     </form>
                                 <?php else : ?>
                                     <p class="font-bold text-red-600">Debe iniciar sesión para comentar el articulo </p>
@@ -207,10 +209,10 @@ session_start() ?>
             </aside>
         <?php endif ?>
         <!-- Esto es para añadir un nuevo comentario -->
-        <div id="insertar_comentario" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+        <div id="comentar" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
             <div class="relative p-4 w-full max-w-md h-full md:h-auto">
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="insertar_comentario">
+                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="comentar">
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                         </svg>
@@ -226,10 +228,10 @@ session_start() ?>
                                 <input id="ocultoId" type="hidden" name="articulo_id">
                                 <input id="ocultoIdUsuario" type="hidden" name="usuario_id">
                             </div>
-                            <button data-modal-toggle="insertar_comentario" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                            <button data-modal-toggle="comentar" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                                 Enviar
                             </button>
-                            <button data-modal-toggle="insertar_comentario" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                            <button data-modal-toggle="comentar" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                                 No, cancelar
                             </button>
                         </form>
